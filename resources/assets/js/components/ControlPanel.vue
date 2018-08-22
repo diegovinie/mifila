35b861a71b96acc2c1d613c5f965b565b20a16ef
Diego Viniegra <diego.viniegra@gmail.com>
2018-08-22 16:30:51 -0500

    front: primera prueba ControlPanel

---
+++ resources/assets/js/components/ControlPanel.vue
@@ -0,0 +1,67 @@
+<template lang="html">
+  <div class="">
+      <div class="title">
+
+      </div>
+      <div class="row">
+          <div class="left">
+
+          </div>
+          <div class="center">
+              <Summary :globals="globals"></Summary>
+          </div>
+          <div class="right">
+
+          </div>
+      </div>
+      <div class="row">
+          <div class="center">
+
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import Summary from './Summary'
+
+export default {
+    data: () => ({
+        globals: {
+            queue: null,
+            cashiers: null,
+            finished: null
+        }
+    }),
+
+    components: {
+        Summary
+    },
+
+    methods: {
+        fetch () {
+            console.log('en fetch')
+            axios.get('/api/v1/globals')
+                .then(({data}) => {
+                    console.log(data)
+                    this.globals = data
+                })
+                .catch(err => {
+                    console.log('error en fetch: ', err)
+                })
+        }
+    },
+
+    created () {
+
+        this.fetch()
+
+        setInterval(() => {
+            this.fetch()
+        }, 5000)
+    }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ resources/assets/js/components/Summary.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="" style="font-size:20px;">
+      <span>Total fila: <strong>{{ globals.queue }}</strong> </span>
+      <br />
+      <span>Total Cajeros: <strong>{{ globals.cashiers }}</strong> </span>
+      <br />
+      <span>Atendidos hoy: <strong>{{ globals.finished }}</strong> </span>
+      <br />
+  </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'globals'
+    ]
+}
+</script>
+
+<style lang="css">
+</style>
