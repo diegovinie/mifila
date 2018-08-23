971b53733221c9ea972e9d83f2271dbb56d8a086
Diego Viniegra <diego.viniegra@gmail.com>
2018-08-23 16:31:02 -0500

    fea: comando del simulador para el cron

--- app/Console/Commands/CheckCashiersSimulator.php
+++ app/Console/Commands/CheckCashiersSimulator.php
@@ -74,37 +74,37 @@ class CheckCashiersSimulator extends Command
         }
     }
 
-    public function handlebk()
-    {
-        $agencies = Agency::all();
-        // $cashiers = Cashier::all();
-
-        foreach ($agencies as $agency) {
-            // code...
-            $cashiers = $agency->cashiers()->get();
-
-            foreach ($cashiers as $cashier) {
-                // code...
-                if ($cashier->idle) {
-                    $url = route('api.cashiers.next', $cashier->id);
-                    $output = shell_exec("curl $url -s");
-                    $service = json_decode($output);
-
-                    try {
-                        if (isset($service->status) && $service->status == 'empty') {
-                            continue;
-                        }
-
-                        $this->timedOutput("En $agency->name el cajero $cashier->name libre, llamando a: {$service->ticket->num}\n");
-
-                    } catch (\Exception $e) {
-                        // var_dump($output);
-                        echo "$cashier->name de $agency->name no se puede recuperar datos del servicio.\n";
-                    }
-                } else {
-                    // echo "$cashier->name Ocupado.\n";
-                }
-            }
-        }
-    }
+    // public function handlebk()
+    // {
+    //     $agencies = Agency::all();
+    //     // $cashiers = Cashier::all();
+    //
+    //     foreach ($agencies as $agency) {
+    //         // code...
+    //         $cashiers = $agency->cashiers()->get();
+    //
+    //         foreach ($cashiers as $cashier) {
+    //             // code...
+    //             if ($cashier->idle) {
+    //                 $url = route('api.cashiers.next', $cashier->id);
+    //                 $output = shell_exec("curl $url -s");
+    //                 $service = json_decode($output);
+    //
+    //                 try {
+    //                     if (isset($service->status) && $service->status == 'empty') {
+    //                         continue;
+    //                     }
+    //
+    //                     $this->timedOutput("En $agency->name el cajero $cashier->name libre, llamando a: {$service->ticket->num}\n");
+    //
+    //                 } catch (\Exception $e) {
+    //                     // var_dump($output);
+    //                     echo "$cashier->name de $agency->name no se puede recuperar datos del servicio.\n";
+    //                 }
+    //             } else {
+    //                 // echo "$cashier->name Ocupado.\n";
+    //             }
+    //         }
+    //     }
+    // }
 }
---
+++ app/Console/Commands/CronSimulator.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Library\QueueManager;
+use App\Library\Simulator;
+
+class CronSimulator extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'simulator:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Correr el simulador desde el crontab.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(QueueManager $queue, Simulator $sim)
+    {
+        //
+
+        $agencies = \App\Agency::all();
+
+        foreach ($agencies as $agency) {
+
+            $cashiers = $agency->cashiers()->get();
+
+            foreach ($cashiers as $cashier) {
+
+                if ($cashier->idle) {
+
+                    $service = $queue->cashierCalls($cashier);
+                    // var_dump($service);
+                    if (!$service) {
+                        continue;
+                    }
+                }
+            }
+        }
+
+        $probNewTicket = $sim->probability(true);
+
+    if(mt_rand(0, $sim->top) < $probNewTicket) {
+
+            $cc = Simulator::genCC();
+
+            $client = $queue->clientFromCC($cc);
+
+            if (!isset($client->name)) {
+
+                $client = Simulator::generateIdentity($cc);
+                $client->save();
+
+                if (!$client) {
+                    return;
+                }
+            }
+
+            $agency = $agencies[mt_rand(0, count($agencies) - 1)];
+
+            if (!($agency instanceof \App\Agency)) {
+                echo "Problemas con la agencia.\n";
+                return;
+            }
+
+            $ticket = $queue->newTicket($client, $agency);
+        }
+    }
+}
