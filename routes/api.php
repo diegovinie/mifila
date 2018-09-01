402f68676112f530523e9094e9d410f98ce420a9
Diego Viniegra <diego.viniegra@gmail.com>
2018-09-01 11:33:48 -0500

    fix: ruta clients corregida

--- routes/api.php
+++ routes/api.php
@@ -17,32 +17,40 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1', 'middleware' => 'throttle:1000,1'], function () {
-    Route::name('api.tickets.list')->get('tickets', 'Api\TicketsController@list');
-    Route::name('api.tickets.store')->post('agencies/{id}/tickets/create', 'Api\TicketsController@store');
+Route::group([
+        'prefix' => 'v1',
+        'as' => 'api.',
+        'middleware' => 'throttle:1000,1'], function () {
 
-    Route::name('api.clients.list')->get('clients', 'Api\ClientsController@list');
-    Route::name('api.clients.show')->get('clients/{cc}', 'Api\ClientsController@show');
-    Route::name('api.clients.checkTicket')->get('clients/{cc}/check', 'Api\ClientsController@check');
-    Route::name('api.sim.gen.client')->get('clients/{cc}/generate', 'Api\ClientsController@generateIdentity');
+    Route::name('tickets.list')->get('tickets', 'Api\TicketsController@list');
+    Route::name('tickets.store')->post('agencies/{id}/tickets/create', 'Api\TicketsController@store');
 
-    Route::name('api.cashiers.next')->get('cashiers/{id}/next', 'Api\CashiersController@next');
+    Route::name('clients.list')->get('clients', 'Api\ClientsController@list');
+    Route::name('clients.show')->get('clients/{cc}', 'Api\ClientsController@show');
+    Route::name('clients.checkTicket')->get('clients/{cc}/check', 'Api\ClientsController@check');
+    Route::name('sim.gen.client')->get('clients/{cc}/generate', 'Api\ClientsController@generateIdentity');
 
-    Route::name('api.globals.all')->get('globals', 'Api\GlobalsController@all');
-    Route::name('api.globals.queue')
+    Route::apiResource('cashiers', 'Api\CashiersController')
+        ->only('index', 'show', 'update');
+    Route::name('cashiers.next')->get('cashiers/{cashier}/next', 'Api\CashiersController@next');
+
+    Route::name('globals.all')->get('globals', 'Api\GlobalsController@all');
+    Route::name('globals.queue')
         ->get('globals/queue', 'Api\GlobalsController@queue');
-    Route::name('api.globals.finished')
+    Route::name('globals.finished')
         ->get('globals/finished', 'Api\GlobalsController@finished');
-    Route::name('api.globals.activeCashier')
+    Route::name('globals.activeCashier')
         ->get('globals/cashiers/active', 'Api\GlobalsController@cashiers');
-    Route::name('api.globals.avg')->get('globals/avg', 'Api\GlobalsController@avg');
+    Route::name('globals.avg')->get('globals/avg', 'Api\GlobalsController@avg');
 
-    Route::name('api.agencies.info')->get('agencies/{id}/info', 'Api\AgenciesController@all');
+    Route::name('agencies.info')->get('agencies/{id}/info', 'Api\AgenciesController@all');
 
-    Route::name('api.ping')->get('ping', function() { return response()->json(1, 200); });
+    Route::name('ping')->get('ping', function() { return response()->json(1, 200); });
 
-    Route::get('configs', 'Api\ConfigController@index');
-    Route::get('configs/{config}', 'Api\ConfigController@show');
-    Route::put('configs/{config}', 'Api\ConfigController@update');
-    Route::get('configs/{config}/items', 'Api\ConfigController@items');
+    Route::apiResource('configs', 'Api\ConfigController')
+        ->only('index', 'show', 'update', 'next');
+    Route::name('configs.items')->get('configs/{config}/items', 'Api\ConfigController@items');
+    // Route::get('configs', 'Api\ConfigController@index');
+    // Route::get('configs/{config}', 'Api\ConfigController@show');
+    // Route::put('configs/{config}', 'Api\ConfigController@update');
 });
